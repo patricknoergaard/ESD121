1fa9e1073d3f707656a0c42f2eaf0505e5cd407e
patricknoergaard <120949856+patricknoergaard@users.noreply.github.com>
2024-09-16 17:58:35 +0000

    test8

--- test/test.c
+++ test/test.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int i, j;
+    int i;
+    int j;
 
     for (i = 0; i <= 1000; i+=5) {
         printf("%i\n", i);
+    }
+
+    for (j = 1; j <=1000; j++) {
+        float x = (float)sqrt(j);
+        printf("%f\n", x);
     }
 }
